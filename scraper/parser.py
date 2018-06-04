6ca671cf7d1326ccd6adca9da850623be9c69854
Gautam Visveswaran <gautam@wireless-173-86-253-76.dr02.roch.ny.frontiernet.net>
2018-06-04 13:15:57 -0400

    fully scraped rferl 2004-2008

--- scraper/parser.py
+++ scraper/parser.py
@@ -23,79 +23,94 @@ class ArticleData():
 
 
 # Thread function takes in URL and collects article data
-def parseArticlesArmenpress(article_urls):
+def parseArticlesArmenpress(article_urls, outfile):
 
 	parsed_articles = []
 
-	for url in article_urls:
+	with open(csv_file, 'wb') as outfile:
+		
+		writer = csv.writer(outfile)
 
-		try:
-			#Get the article 
-			article_html = get(url)
-	
-			#Parse with beautifulsoup
-			article_soup = BeautifulSoup(article_html.text, "html.parser")
 
-			#Find title and body of each article
-			title = article_soup.find(class_="opennewstitle").contents[0]
-			body = article_soup.find(itemprop="articleBody").contents[0]
-			#Remove HTML tags and special characters
-			title = re.sub(re.compile('<.*?>'), '', title.encode('utf-8'))
-			title = re.sub('[^ a-zA-Z0-9]', '', title)
-			body = re.sub(re.compile('<.*?>'), '', body.encode('utf-8'))
-			body = re.sub('[^ a-zA-Z0-9]', '', body)
-			
-			#TODO: parse other info
+		for url in article_urls:
 
-			#Add to result list
-			parsed_articles.append(ArticleData(url, title, body))
+			try:
+				#Get the article 
+				article_html = get(url)
+		
+				#Parse with beautifulsoup
+				article_soup = BeautifulSoup(article_html.text, "html.parser")
 
-		except Exception as e:
-			print e
-			pass
+				#Find title and body of each article
+				title = article_soup.find(class_="opennewstitle").contents[0]
+				body = article_soup.find(itemprop="articleBody").contents[0]
+				#Remove HTML tags and special characters
+				title = re.sub(re.compile('<.*?>'), '', title.encode('utf-8'))
+				title = re.sub('[^ a-zA-Z0-9]', '', title)
+				body = re.sub(re.compile('<.*?>'), '', body.encode('utf-8'))
+				body = re.sub('[^ a-zA-Z0-9]', '', body)
+				
+
+				#Add to result list
+				parsed_articles.append(ArticleData(url, title, body))
+
+
+
+			except Exception as e:
+				print e
+				pass
 
 	return parsed_articles
 
 
-def parseArticlesRFERL(article_urls):
+def parseArticlesRFERL(article_urls, outfile):
 
 	parsed_articles = []
 
-	for url in article_urls:
-	# for i in range(0,10):
-
-		try:
-
-			#Get the article 
-			article_html = get(url)
-			# article_html = get(article_urls[i])
-	
-			#Parse with beautifulsoup
-			article_soup = BeautifulSoup(article_html.text, "html.parser")
+	with open(csv_file, 'wb') as outfile:
+		
+		writer = csv.writer(outfile)
 
 
+		for url in article_urls:
+		# for i in range(0,10):
 
-			#Get content and title
-			body = article_soup.find(class_="wsw").contents[0]
-			title = article_soup.find(class_="pg-title").contents[0]
-			date = str(article_soup.find("time").contents[0].strip())
-			summary = str(article_soup.find(True, {'class':['intro', 'content-offset']}).find(class_="wsw").text)
+			try:
 
-
-			title = re.sub(re.compile('<.*?>'), '', title.encode('utf-8'))
-			title = re.sub('[^ a-zA-Z0-9]', '', title)
-			body = re.sub(re.compile('<.*?>'), '', body.encode('utf-8'))
-			body = re.sub('[^ a-zA-Z0-9]', '', body)
-			summary = re.sub(re.compile('<.*?>'), '', summary.encode('utf-8'))
-			summary = re.sub('[^ a-zA-Z0-9]', '', summary)
+				#Get the article 
+				article_html = get(url)
+				# article_html = get(article_urls[i])
+		
+				#Parse with beautifulsoup
+				article_soup = BeautifulSoup(article_html.text, "html.parser")
 
 
-			parsed_articles.append(ArticleData(url, title, date, body, summary))
-			# parsed_articles.append(ArticleData(article_urls[i], title, date, body, summary))
 
-		except Exception as e:
-			print e
-			pass
+				#Get content and title
+				body = article_soup.find(class_="wsw").contents[0]
+				title = article_soup.find(class_="pg-title").contents[0]
+				date = str(article_soup.find("time").contents[0].strip())
+				summary = str(article_soup.find(True, {'class':['intro', 'content-offset']}).find(class_="wsw").text)
+
+
+				title = re.sub(re.compile('<.*?>'), '', title.encode('utf-8'))
+				title = re.sub('[^ a-zA-Z0-9]', '', title)
+				body = re.sub(re.compile('<.*?>'), '', body.encode('utf-8'))
+				body = re.sub('[^ a-zA-Z0-9]', '', body)
+				summary = re.sub(re.compile('<.*?>'), '', summary.encode('utf-8'))
+				summary = re.sub('[^ a-zA-Z0-9]', '', summary)
+
+
+				article = ArticleData(url, title, date, body, summary)
+				parsed_articles.append(article)
+				writer.writerow([article.url, article.title, article.date, article.body, article.summary])
+				# parsed_articles.append(ArticleData(article_urls[i], title, date, body, summary))
+
+				
+
+			except Exception as e:
+				print e
+				pass
 
 	return parsed_articles
 
@@ -114,17 +129,10 @@ if __name__ == "__main__":
 	if website.lower() == "armenpress":
 		parsed_articles = parseArticlesArmenpress(article_urls)
 	elif website.lower() == "rferl":
-		parsed_articles = parseArticlesRFERL(article_urls)
+		parsed_articles = parseArticlesRFERL(article_urls, csv_file)
 
 
-	#Write info to CSV file
-	with open(csv_file, 'wb') as outfile:
 			
-		writer = csv.writer(outfile)
-		writer.writerow(['title', 'summary'])
-
-		for article in parsed_articles:
-			writer.writerow([article.url, article.title, article.date, article.body, article.summary])
 
 
 	#TODO thread to improve speed?
